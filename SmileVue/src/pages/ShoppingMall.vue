6b7bdc116ec7ad877ef6bef9029626d509828990
lenovo <buqian0627@163.com>
2019-07-18 11:05:56 +0800

    首页导航栏的制作

--- SmileVue/src/pages/ShoppingMall.vue
+++ SmileVue/src/pages/ShoppingMall.vue
@@ -1,12 +1,17 @@
 <template>
-    <div>
-      {{msg}}
-      <van-row>
-        <van-col span="8">span: 8</van-col>
-        <van-col span="8">span: 8</van-col>
-        <van-col span="8">span: 8</van-col>
-      </van-row>
-    </div>
+  <div class="search-bar">
+    <van-row>
+      <van-col span="3" class="location-icon">
+        <img src="../assets/images/location.png" alt="">
+      </van-col>
+      <van-col span="16">
+        <input type="text" class="search-input">
+      </van-col>
+      <van-col span="5" class="search-btn">
+        <van-button size="mini">查找</van-button>
+      </van-col>
+    </van-row>
+  </div>
 </template>
 
 <script>
@@ -14,12 +19,36 @@ export default {
   name: 'ShoppingMall',
   data () {
     return {
-      msg: 'Shopping Mall'
+      // msg: 'Shopping Mall'
     }
   }
 }
 </script>
 
 <style scoped>
-
+.search-bar {
+  height: 2.2rem;
+  background-color: #e5017d;
+  line-height: 2.2rem;
+}
+.search-input {
+  width: 100%;
+  height: 1.3rem;
+  background-color: #e5017d;
+  color: #fff;
+  border-top: 0;
+  border-left: 0;
+  border-right: 0;
+  border-bottom: 1px solid #fff;
+}
+.location-icon {
+  padding-left: .7rem;
+  padding-top: .1rem;
+}
+.location-icon img {
+  width: 50%;
+}
+.search-btn {
+  text-align: center;
+}
 </style>
